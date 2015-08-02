14822195bddb3112786a8b89ad44c11c0f7c97c1
Kennesaw Sate Aerial robotics <bhopewel@spsu.edu>
2015-08-02 15:22:19 -0400

    remove mavros from source control, add laser scan splitter node, refactor of loiter to use hokuyo

--- spsuart/src/of_loiter_test.cpp
+++ spsuart/src/of_loiter_test.cpp
@@ -1,6 +1,7 @@
 #include "spsuart/autonomous.h"
 #include "px_comm/OpticalFlow.h"
 #include <termios.h>
+#include "sensor_msgs/LaserScan.h"
 
 using namespace std;
 using namespace cv;
@@ -13,6 +14,8 @@ int pitch = MID_PWM;
 
 double target_altitude = 1.5;
 
+double ground_distance;
+
 // Instantiate PID controllers
 PIDController* xVelCtrl = new PIDController(1,0,0,-100,100);
 PIDController* yVelCtrl = new PIDController(1,0,0,-100,100);
@@ -20,6 +23,7 @@ PIDController* altPosCtrl = new PIDController(500,0,0,-300,300);
 mavlink_message_t* msgt = NULL;
 __mavlink_rangefinder_t* x = NULL;
 
+/*
 void apmMavlinkmsgCallback(const mavros::Mavlink::ConstPtr& msg){
         if(msg->msgid==173){
 
@@ -50,18 +54,36 @@ void apmMavlinkmsgCallback(const mavros::Mavlink::ConstPtr& msg){
                 x = NULL;
         }
 }
+*/
+
+void splitScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {	
+	double avg = 0;
+	for(int i = 0; i < sizeof(msg->ranges)/4; i++) {
+		if(msg->ranges[i] < 4){
+			avg += (double)(msg->ranges[i]/(sizeof(msg->ranges)/4));
+		}
+		else{	
+			avg += (double)(4/(sizeof(4)));
+		}
+	}
+	ground_distance = avg;	
+	cout<<"Altitude :"<<avg<<endl;                
+	double out = altPosCtrl->calc(ground_distance);
+    cout << "PID Out: " << out << endl;
+	throttle = MID_PWM + out;
+}
 
 void optFlowCallback(const px_comm::OpticalFlow::ConstPtr& msg) 
 { 
-   double vel_x = msg->velocity_x;
-   double vel_y = msg->velocity_y;
+   //double vel_x = msg->velocity_x;
+   // double vel_y = msg->velocity_y;
    // pos_z = msg->ground_distance; 
    
-   double x_out = xVelCtrl->calc(vel_x);
-   double y_out = yVelCtrl->calc(vel_y);
+   // double x_out = xVelCtrl->calc(vel_x);
+   // double y_out = yVelCtrl->calc(vel_y);
 
-   roll = MID_PWM + x_out;
-   pitch = MID_PWM + y_out; 
+   //roll = MID_PWM + x_out;
+   //pitch = MID_PWM + y_out; 
 }
 
 
@@ -120,8 +142,8 @@ int main(int argc, char **argv)
   
   //Image and mavlink message subscriber
   ros::Subscriber subflow = n.subscribe("px4flow/opt_flow",1,optFlowCallback);
-  ros::Subscriber submav = n.subscribe("mavlink/from", 1, apmMavlinkmsgCallback);
-  
+  //ros::Subscriber submav = n.subscribe("mavlink/from", 1, apmMavlinkmsgCallback);
+  ros::Subscriber subHokuyo = n.subscribe("scan3", 1, splitScanCallback); 
   //Mavros rc override publisher
   ros::Publisher rc_pub = n.advertise<mavros::OverrideRCIn>("/mavros/rc/override", 1);
   
@@ -130,8 +152,8 @@ int main(int argc, char **argv)
   ros::Rate fcuCommRate(45); // emulating speed of dx9 controller
   
   // Set PID controller targets  
-  xVelCtrl->targetSetpoint(0); // target X coordinate in pixels
-  yVelCtrl->targetSetpoint(0); // target Y coordinate in pixels
+  // xVelCtrl->targetSetpoint(0); // target X coordinate in pixels
+  // yVelCtrl->targetSetpoint(0); // target Y coordinate in pixels
   altPosCtrl->targetSetpoint(target_altitude); // target altitude in meters
   int inputChar = 'a';
   
@@ -140,15 +162,11 @@ int main(int argc, char **argv)
     //While node is alive send RC values to the FC @ fcuCommRate hz
     while(ros::ok()){
 
-            if(!altPosCtrl->isSettled() || land) {
-			    msg.channels[ROLL_CHANNEL]=msg.CHAN_RELEASE;
-			    msg.channels[PITCH_CHANNEL]=msg.CHAN_RELEASE;
-			}
-            else {
-                msg.channels[ROLL_CHANNEL]=roll;
-			    msg.channels[PITCH_CHANNEL]=pitch;
-            }
-            msg.channels[THROTTLE_CHANNEL]=throttle;
+            //msg.channels[ROLL_CHANNEL]=roll;
+			//msg.channels[PITCH_CHANNEL]=pitch;
+            msg.channels[ROLL_CHANNEL]=msg.CHAN_RELEASE;
+			msg.channels[PITCH_CHANNEL]=msg.CHAN_RELEASE;
+			msg.channels[THROTTLE_CHANNEL]=throttle;
 			msg.channels[YAW_CHANNEL]=msg.CHAN_RELEASE;
 			msg.channels[MODE_CHANNEL]=ALT_HOLD_MODE;
 			msg.channels[NOT_USED_CHANNEL]=msg.CHAN_RELEASE;
