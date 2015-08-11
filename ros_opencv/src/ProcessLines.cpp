5b9c38e406f17af55a35d7a2266d6a14c4ed7346
Kennesaw Sate Aerial robotics <bhopewel@spsu.edu>
2015-08-11 15:30:38 -0400

    refactor grid detection

--- ros_opencv/src/ProcessLines.cpp
+++ ros_opencv/src/ProcessLines.cpp
@@ -41,81 +41,154 @@ public:
    : it_(nh_)
  {
    result_pub= nh_.advertise<ros_opencv::TrackingPoint>("boundaryinfo" , 1);
-   image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, &ColorDetector::imageCb, this);
+   image_sub_ = it_.subscribe("image_raw", 1, &ColorDetector::imageCb, this);
  }
 
  ~ColorDetector()
  {
 
  }
+ 
+ 
 
-Mat GetThresholdedImage(Mat img, _InputArray lowBound, _InputArray upBound)
+
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+double angle( Point pt1, Point pt2, Point pt0 )
 {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
+ 
+void findSquares( Mat& image, vector<vector<Point> >& squares )
+{
+	int thresh = 50, N = 11;
+const char* wndname = "Square Detection Demo";
 
-Mat imgHSV(img.rows,img.cols, CV_8UC3, Scalar(0,0,0));
+    squares.clear();
 
-cvtColor(img, imgHSV, CV_BGR2HSV);
+    Mat pyr, timg, gray0(image.size(), CV_8U), gray;
 
-Mat imgThreshed(img.rows,img.cols, CV_8UC3, Scalar(0,0,0));
+    // down-scale and upscale the image to filter out the noise
+    pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
+    pyrUp(pyr, timg, image.size());
+    vector<vector<Point> > contours;
 
-inRange((_InputArray)imgHSV, lowBound, upBound, imgThreshed);
+    // find squares in every color plane of the image
+    for( int c = 0; c < 1; c++ )
+    {
+        int ch[] = {c, 0};
+        mixChannels(&timg, 1, &gray0, 1, ch, 1);
+        
 
-imgHSV.release();
 
-return imgThreshed;
+        // try several threshold levels
+        for( int l = 0; l < N; l++ )
+        {
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if( l == 0 )
+            {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+                Canny(gray0, gray, 0, thresh, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                dilate(gray, gray, Mat(), Point(-1,-1));
+            }
+            else
+            {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l+1)*255/N;
+            }
+
+            // find contours and store them all as a list
+            findContours(gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+            vector<Point> approx;
+            
+            // test each contour
+            for( size_t i = 0; i < contours.size(); i++ )
+            {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                if( approx.size() == 4 &&
+                    fabs(contourArea(Mat(approx))) > 1000 &&
+                    isContourConvex(Mat(approx)) && fabs(contourArea(Mat(approx))) < 100000)
+                {
+                    double maxCosine = 0;
+
+                    for( int j = 2; j < 5; j++ )
+                    {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if( maxCosine < 0.3 ){
+                        squares.push_back(approx);						
+						}
+					}
+                }
+
+            }
+        }
 }
 
-//Night Led webcam
-//~ Mat getBoundary(Mat src)
-//~ {
-	//~ Mat fullRedThresh;
-	//~ Mat fullGreenThresh;
-	//~ Mat fullWhiteThresh;
-	//~ Mat fullFinalThresh;
-//~ 
-	//~ Mat imgGreenHighThresh; inRange(src, Scalar(40,20,235), Scalar(80,125,255),imgGreenHighThresh);
-    //~ Mat imgGreenLowThresh; inRange(src, Scalar(50,55,210), Scalar(85,110,255),imgGreenLowThresh);
-	//~ Mat imgRedHighThresh; inRange(src, Scalar(50,60,215), Scalar(195,125,255),imgRedHighThresh);
-	//~ Mat imgRedLowThresh; inRange(src, Scalar(0,50,220), Scalar(10,125,255),imgRedLowThresh);
-    //~ Mat imgWhiteLowThresh; inRange(src, Scalar(0,0,200), Scalar(5,5,255),imgWhiteLowThresh);
-    //~ Mat imgWhiteMidThresh; inRange(src, Scalar(60,0,200), Scalar(120,35,255),imgWhiteMidThresh);
-    //~ Mat imgWhiteHighThresh; inRange(src, Scalar(200,5,200), Scalar(255,15,255),imgWhiteHighThresh);
-//~ 
-    //~ bitwise_or(imgGreenHighThresh,imgGreenLowThresh,fullGreenThresh);
-	//~ bitwise_or(imgRedHighThresh,imgRedLowThresh,fullRedThresh);
-	//~ bitwise_or(imgWhiteLowThresh,imgWhiteHighThresh,fullWhiteThresh);
-    //~ bitwise_or(fullGreenThresh,fullRedThresh,fullFinalThresh);
-    //~ bitwise_or(fullFinalThresh,fullWhiteThresh,fullFinalThresh);
-    //~ bitwise_or(fullFinalThresh,imgWhiteMidThresh,fullFinalThresh);
-//~ 
-	//~ erode(fullFinalThresh, fullFinalThresh, Mat(), Point(-1,-1),2,1,1);
-//~ 
-	//~ return fullFinalThresh;
-//~ }
 
-//HD webcam
-Mat getBoundary(Mat src)
+// the function draws all the squares in the image
+void drawSquares( Mat& image, const vector<vector<Point> >& squares )
 {
-Mat fullRedThresh;
-Mat fullGreenThresh;
-Mat fullFinalThresh;
+	int thresh = 50, N = 11;
+const char* wndname = "Square Detection Demo";
+	
+	int avgPosX=0;
+	int avgPosY=0;
+    
+    for( size_t i = 0; i < squares.size(); i++ )
+    {
+        const Point* p = &squares[i][0];
+        int n = (int)squares[i].size();
+        polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, 8, 0);
+        Moments m = moments(squares[i]);
+		avgPosX += m.m10/m.m00;
+		avgPosY += m.m01/m.m00;
+    }
+    
+    if(squares.size() != 0)
+	{
+		avgPosX /= squares.size();
+		avgPosY /= squares.size();
+		circle(image, Point(avgPosX,avgPosY), 30, cvScalar(255,0,0), 5, 8, 0);
+	}
+	else{
+		avgPosX = -1;
+		avgPosY = -1;
+	}
+	
+	ros_opencv::TrackingPoint trackingPoint;
+	trackingPoint.pointX=avgPosX;
+	trackingPoint.pointY=avgPosY;
+	result_pub.publish(trackingPoint);
 
-Mat imgGreenHighThresh; inRange(src, Scalar(75,20,235), Scalar(105,125,255),imgGreenHighThresh);
-    Mat imgGreenLowThresh; inRange(src, Scalar(60,120,210), Scalar(85,185,255),imgGreenLowThresh);
-Mat imgRedHighThresh; inRange(src, Scalar(50,60,215), Scalar(195,125,255),imgRedHighThresh);
-Mat imgRedLowThresh; inRange(src, Scalar(0,50,220), Scalar(10,125,255),imgRedLowThresh);
-Mat imgWhiteThresh; inRange(src, Scalar(0,0,230), Scalar(5,5,255),imgWhiteThresh);
-
-    bitwise_or(imgGreenHighThresh,imgGreenLowThresh,fullGreenThresh);
-bitwise_or(imgRedHighThresh,imgRedLowThresh,fullRedThresh);
-    bitwise_or(fullGreenThresh,fullRedThresh,fullFinalThresh);
-    bitwise_or(fullFinalThresh,imgWhiteThresh,fullFinalThresh);
-
-erode(fullFinalThresh, fullFinalThresh, Mat(), Point(-1,-1),2,1,1);
-
-return fullFinalThresh;
+    imshow(wndname, image);
 }
-
 
  void imageCb(const sensor_msgs::ImageConstPtr& msg)
  {
@@ -133,47 +206,15 @@ return fullFinalThresh;
 Mat frame=cv_ptr->image;
 Mat frameSmall;
 resize(frame, frameSmall, Size(640,480));
-Mat hsv;
-cvtColor(frameSmall, hsv, CV_BGR2HSV);
-Mat finalImage = getBoundary(hsv);
 
+		vector<vector<Point> > squares;
+        findSquares(frameSmall, squares);
+        drawSquares(frameSmall, squares);
 
+imshow("ColorTrackerRGB", frameSmall);
+waitKey(1);
 
-IplImage iplimg =finalImage;
-
-// Calculate the moments to estimate the position of the color
-CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments));
-cvMoments(&iplimg, moments, 1);
-
-// The actual moment values
-double moment10 = cvGetSpatialMoment(moments, 1, 0);
-double moment01 = cvGetSpatialMoment(moments, 0, 1);
-double area = cvGetCentralMoment(moments, 0, 0);
-
-int posX;
-int posY;
-
-if(area>50){
- posX = moment10/area;
- posY = moment01/area;
 }
-else{
-posX=-1;
-posY=-1;
-}
-
-//circle(frameSmall, Point(posX,posY), 30, cvScalar(255,0,0), 5, 8, 0);
-
-//imshow("ColorTrackerRGB", frameSmall);
-//imshow("ColorTrackerThresh", finalImage);
-//waitKey(30);
-
-boundmsg.pointX=posX;
-boundmsg.pointY=posY;
-
-result_pub.publish(boundmsg);
-
-}   
 };
 
 int main(int argc, char** argv)
