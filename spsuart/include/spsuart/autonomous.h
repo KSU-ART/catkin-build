c925ac4e55b370005ebca730fc105e6fd3af6ab8
Alec Graves <alec0061@hotmail.com>
2016-06-21 23:04:09 -0400

    moved some things around

--- spsuart/include/spsuart/autonomous.h
+++ spsuart/include/spsuart/autonomous.h
@@ -11,7 +11,6 @@
 #include "opencv/cv.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/features2d/features2d.hpp"
-#include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
