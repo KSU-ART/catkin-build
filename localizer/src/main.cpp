77f56285e630ce5555e87bc260b723fece72b676
stoplime <slanimero@gmail.com>
2016-06-29 16:30:47 -0400

    fixed commit collisions

--- localizer/src/main.cpp
+++ localizer/src/main.cpp
@@ -1,11 +1,6 @@
 #include "loc_object.h"
-<<<<<<< HEAD
-//#include <Eigen/Geometry> 
-
-
-=======
 #include "grid_tracker.h"
->>>>>>> 281e31141dce61d812232b3a3b8da1586450e658
+
 int main()
 {
 	grid_tracker gt_1;
