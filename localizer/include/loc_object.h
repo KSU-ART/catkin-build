d666c22ac321cd685c0803bc5ffa0f29d8ebc191
stoplime <slanimero@gmail.com>
2016-06-30 00:37:35 -0400

    added math quaternions

--- localizer/include/loc_object.h
+++ localizer/include/loc_object.h
@@ -15,6 +15,9 @@
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/Imu.h>
 
+#include "quaternion.h"
+#include "vector.h"
+
 
 class sensor_processor
 {
@@ -24,8 +27,10 @@ private:
 	ros::Subscriber sub_guidance_velocity, sub_guidance_imu, sub_guidance_sonar, sub_pixhawk_imu, sub_hokuyo;
 	
 	/// sensor fused data
-	geometry_msgs::Point pose_fused;
-	geometry_msgs::Quaternion orientation_fused;
+	geometry_msgs::Point pose_fused_msg;
+	geometry_msgs::Quaternion orientation_fused_msg;
+	//geometry_msgs::Point pose_fused;
+	Quaternion orientation_fused;
 	
 	/// passed values through callbacks
 	geometry_msgs::Point vel_pixhawk_imu;
@@ -33,11 +38,17 @@ private:
 	geometry_msgs::Quaternion orient_pixhawk_imu;
 	
 	/// Prevalues for integration;
-	geometry_msgs::Vector3Stamped prev_guidance_imu_acc;
-	geometry_msgs::Vector3Stamped prev_pixhawk_imu_acc;
+	geometry_msgs::Vector3 prev_guidance_imu_acc;
+	geometry_msgs::Vector3 prev_pixhawk_imu_acc;
 	
 	geometry_msgs::Vector3Stamped guidance_vel_estimation;
 	geometry_msgs::Vector3Stamped pixhawk_vel_estimation;
+	
+	geometry_msgs::Vector3Stamped global_guidance_vel_estimation;
+	geometry_msgs::Vector3Stamped global_pixhawk_vel_estimation;
+	
+	double sonar_altitude;
+	double fused_altitude;
 	
 	bool vel_reset_zero;
 	
@@ -56,18 +67,18 @@ public:
 	/// Integrate acceleration
 	/// passin fusion orientation
 	/// calculate Global velocity
-	void guidance_imu_callback(const geometry_msgs::TransformStamped& msg);
+	void guidance_imu_callback(const geometry_msgs::TransformStamped::ConstPtr& msg);
 	
 	/// Pass altitude value to Hokuyo
-	void guidance_sonar_callback(const sensor_msgs::LaserScan& msg);
+	void guidance_sonar_callback(const sensor_msgs::LaserScan::ConstPtr& msg);
 	
 	/// Integrate to velocity
 	/// Set to global referance
 	/// Sent value to guidance IMU
-	void pixhawk_imu_callback(const sensor_msgs::Imu& msg);
+	void pixhawk_imu_callback(const sensor_msgs::Imu::ConstPtr& msg);
 	
 	/// fuse data with sonar
-	void hokuyo_sub(const sensor_msgs::LaserScan& msg);
+	void hokuyo_sub(const sensor_msgs::LaserScan::ConstPtr& msg);
 	
 };
 
