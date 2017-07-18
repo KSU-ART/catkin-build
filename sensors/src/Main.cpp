7ab910ccc3f945be7e99e389bee32f1fed49cce8
Kyle Pawlowski <kylepawlowski1@gmail.com>
2017-07-18 13:31:01 -0400

    add method to convert quaternions to 2D heading

--- sensors/src/Main.cpp
+++ sensors/src/Main.cpp
@@ -1,8 +1,23 @@
 #include "ros/ros.h"
 #include "sensors.h"
+#include <cmath>
 
 using namespace std;
 
+float convertQuat(std::array<double, 4> quat){
+	// made by Kyle
+	// returns double representing radians rotated in x-z plane between -PI and PI
+	// with the positive direction being counter-clockwise
+	// don't ask me how it works. it's magic
+	double test = quat[0]*quat[1] + quat[2]*quat[3];
+	if(test > 0.499){
+		return 2*atan2(quat[0], quat[3]);
+	}
+	else if(test < -0.499){
+		return -2*atan2(quat[0], quat[3]);
+	}
+	return atan2(2*quat[1]*quat[3] - 2*quat[0]*quat[2], 1 - 2*pow(quat[1],2.0) - 2*pow(quat[2],2.0));
+}
 
 int main(int argc, char** argv)
 {
