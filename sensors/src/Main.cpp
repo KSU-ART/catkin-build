86104e0eb526d11ca8eddd8cafbe8dcd88a38381
stoplime <slanimero@gmail.com>
2017-07-19 13:45:15 -0400

    blob_tetect to un compressed

--- sensors/src/Main.cpp
+++ sensors/src/Main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "SensorMain");
 	LidarSensor sensorHokuyo(0, "/scan");
 	LidarSensor sensorAltitudeLidar(1, "/terarangerone");
-	// imageEncoder forwardCam(1, "/sensor/forwardCam");
-	imageEncoder forwardCam(0, "/sensor/downCam");
+	// imageEncoder forwardCam(1, "/sensor/compressed/forwardCam");
+	imageEncoder forwardCam(1, "/sensor/compressed/downCam");
 
 	ros::Rate loop_rate(120);
 	while (ros::ok())
